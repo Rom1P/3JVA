01bb97079e7fcbcc59bedd124496085304f6c85f
Thomas WEBER <223631@supinfo.com>
2019-04-01 11:27:51 +0200

    Up

--- src/entities/Picture.java
+++ src/entities/Picture.java
@@ -6,4 +6,35 @@ public class Picture {
     private String date;
     private String categories;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
 }
